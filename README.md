e08cc2ef5b79c5936bb257a3a5b13a7409af46ec
Timothy Hull <timothyhull@gmail.com>
2021-05-20 06:56:40 +0000

    Day 6d

--- README.md
+++ README.md
@@ -49,6 +49,7 @@ My log :clipboard: and notes​ :notebook: for thethe **TalkPython #100DaysOfCod
 - Day 6b: [5/16/21](days/6#notebook-51621)
 - Day 6c: [5/17/21](days/6#notebook-51721)
 - Day 6d: [5/18/21](days/6#notebook-51821)
+- Day 6e: [5/19/21](days/6#notebook-51921)
 
 </details>
 
--- days/6/light_sorter.py
+++ days/6/light_sorter.py
@@ -33,7 +33,7 @@ with open(file=SOURCE_DATA_FILE,
     all_lights = load(file)
 
 # Create namedtuple class to store data for a light
-LightData = namedtuple('LightData', 'id name modes')
+LightData = namedtuple('LightData', 'name id modes')
 
 # Create a list object to store namedtuples of light data
 lights = []
@@ -42,18 +42,21 @@ lights = []
 for key, value in all_lights.items():
     # Determine whether or not the light supports enhanced modes
     if value['type'] in light_types['enhanced']['names']:
-        light_modes = light_types['enhanced']['modes']
+        light_mode = light_types['enhanced']['modes']
     else:
-        light_modes = light_types['basic']['modes']
+        light_mode = light_types['basic']['modes']
 
     # Add a namedtuple of light data to the 'lights' list
     lights.append(
         LightData(
-            id=key,
             name=value.get('name', None),
-            modes=light_modes
+            id=key,
+            modes=light_mode
         )
     )
+
+# Sort the list by the first attribute name in the LightData (name)
+lights.sort()
 
 # Find the light with the longest name, to format the output
 name_len = []
@@ -74,4 +77,4 @@ print(f'{"-" * len(header)}')
 for index, light in enumerate(lights):
     print(f'{index + 1:>2}.  '
           f'{light.name:<{spaces}} '
-          f'{", ".join(light.modes)}')
+          f'{", ".join(light.modes).title()}')
