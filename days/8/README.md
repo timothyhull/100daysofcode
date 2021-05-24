7e9a759cd67e2c6e8f9d676f6a7cc5da42067b07
Timothy Hull <timothyhull@gmail.com>
2021-05-24 06:21:08 +0000

    Initial commit

---
+++ days/8/README.md
@@ -0,0 +1,25 @@
+## :calendar: Day 8: 5/23/21
+
+---
+
+## Topics:
+
+:clipboard: Python data structures
+
+---
+
+## Resources:
+
+:star: [PyBite 21](https://codechalleng.es/bites/21/): Query a nested data structure
+
+---
+
+## Tasks:
+
+:white_check_mark: Complete PyBite 21 - **5/23/21**
+
+---
+
+## Notes:
+
+N/A
---
+++ days/8/cars.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+# Cars dictionary
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+
+
+def get_all_jeeps(cars=cars):
+    """return a comma  + space (', ') separated string of jeep models
+       (original order)"""
+    jeeps = cars.get('Jeep')
+    jeeps_string = ', '.join(jeeps)
+
+    return jeeps_string
+
+
+def get_first_model_each_manufacturer(cars=cars):
+    """return a list of matching models (original ordering)"""
+    car_list = [car[0] for car in cars.values()]
+
+    return car_list
+
+
+def get_all_matching_models(cars=cars, grep='trail'):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    car_list = []
+    for car in cars.values():
+        for model in car:
+            if grep.lower() in model.lower():
+                car_list.append(model)
+
+    car_list.sort()
+
+    return car_list
+
+
+def sort_car_models(cars=cars):
+    """return a copy of the cars dict with the car models (values)
+       sorted alphabetically"""
+    cars_sorted = {}
+    for make, models in cars.items():
+        cars_sorted[make] = [m for m in models]
+        cars_sorted[make].sort()
+
+    return cars_sorted
