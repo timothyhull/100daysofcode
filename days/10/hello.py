973f10749c4c1b6e91e2a9a57a6e095b66c61d32
Timothy Hull <timothyhull@gmail.com>
2021-05-28 06:44:51 +0000

    Relocated or renamed

--- days/10/hello.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-# Program to test
-def hello_name(name):
-    return f'Hello {name.title()}'
--- days/10/test_hello_name_pytest.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# Import the function to test
-from hello import hello_name
-
-
-# Define a function for code testing
-def test_hello_name():
-    assert hello_name('tim') == 'Hello Tim'
