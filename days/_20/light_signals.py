8413c11fef0b353ca943f53e0dd2e9d3a9b183cf
Timothy Hull <timothyhull@gmail.com>
2021-08-08 07:09:59 +0000

    Revise class method structure

--- days/_20/light_signals.py
+++ days/_20/light_signals.py
@@ -11,72 +11,82 @@ YELLOW_ANSI = '48;5;11'
 GREEN_ANSI = '48;5;10'
 
 
-# ANSI escape sequence for text formatting
-def esc(code: Union[str, int]) -> str:
-    """ Formats a string using an ANSI escape code, to colored/formatted output.
-        ANSI code reference: https://en.wikipedia.org/wiki/ANSI_escape_code
-
-    Args:
-        code:
-            Option #1 (str):
-                Single ANSI code or semicolon-separated list of codes.
-            Option #2 (int):
-                Any single ANSI code.
-
-        Example:
-            code = '48;5;9;38;5;15'
-
-    Prints:
-        None.
-
-    Returns:
-        code (str): ANSI-formatted escape code.
-
-        Example:
-            \033[48;5;9;38;5;15m
-    """
-
-    ansi_code = f'\033[{code}m'
-
-    return ansi_code
-
-
-def activate_light(ansi_color: str = RED_ANSI) -> str:
-    """ Create a traffic light color block.
-
-        Args:
-            ansi_color (str):
-                ANSI-formatted background color string.
-
-        Returns:
-            light (str):
-                Colored active light string
-
-    """
-
-    light = f"""
-| {esc(ansi_color)}      {esc(0)} |
-| {esc(ansi_color)}      {esc(0)} |
-""".strip()
-
-    return light
-
-
 class TrafficLights:
-    """
+    """ Object to display colored ASCII traffic lights.
+
+        Usage:
+            # Instantiate a TrafficLights object:
+                lights = TrafficLights()
+
+            # Use color methods to display a light:
+                lights.red()
     """
 
     def __init__(self) -> None:
         # Create traffic lights
         pass
 
-    def red_light(self):
+    # ANSI escape sequence for text formatting
+    def __esc__(self, code: Union[str, int]) -> str:
+        """ Formats a string using an ANSI escape code, to colored/formatted output.
+            ANSI code reference: https://en.wikipedia.org/wiki/ANSI_escape_code
+
+        Args:
+            code:
+                Option #1 (str):
+                    Single ANSI code or semicolon-separated list of codes.
+                Option #2 (int):
+                    Any single ANSI code.
+
+            Example:
+                code = '48;5;9;38;5;15'
+
+        Returns:
+            code (str): ANSI-formatted escape code.
+
+            Example:
+                \033[48;5;9;38;5;15m
         """
+
+        ansi_code = f'\033[{code}m'
+
+        return ansi_code
+
+    def __activate_light__(self, ansi_color: str) -> str:
+        """ Create a traffic light color block to fill an ASCII trafic light
+            placeholder graphic.
+
+            Args:
+                ansi_color (str):
+                    ANSI-formatted background color string.
+
+            Returns:
+                light (str):
+                    Colored active light string
+
+        """
+
+        light = f"""
+| {self.__esc__(ansi_color)}      {self.__esc__(0)} |
+| {self.__esc__(ansi_color)}      {self.__esc__(0)} |
+    """.strip()
+
+        return light
+
+    def red(self) -> None:
+        """ Method of a TrafficLights object that displays the ASCII graphic
+            for a red traffic light.
+
+            Args:
+                None.
+
+            Returns:
+                None.
         """
 
         self.red_light = f"""
 |¯¯¯¯¯¯¯¯|
-{activate_light(ansi_color=RED_ANSI)}
+{self.__activate_light__(ansi_color=RED_ANSI)}
 |        |
 | |¯¯¯¯| |
 | |____| |
@@ -87,15 +97,23 @@ class TrafficLights:
 """
         print(self.red_light)
 
-    def yellow_light(self):
+    def yellow(self) -> None:
+        """ Method of a TrafficLights object that displays the ASCII graphic
+            for a yellow traffic light.
+
+            Args:
+                None.
+
+            Returns:
+                None.
         """
-        """
+
         self.yellow_light = f"""
 |¯¯¯¯¯¯¯¯|
 | |¯¯¯¯| |
 | |____| |
 |        |
-{activate_light(ansi_color=YELLOW_ANSI)}
+{self.__activate_light__(ansi_color=YELLOW_ANSI)}
 |        |
 | |¯¯¯¯| |
 | |____| |
@@ -103,8 +121,15 @@ class TrafficLights:
 """
         print(self.yellow_light)
 
-    def green_light(self):
-        """
+    def green(self) -> None:
+        """ Method of a TrafficLights object that displays the ASCII graphic
+            for a green traffic light.
+
+            Args:
+                None.
+
+            Returns:
+                None.
         """
 
         self.green_light = f"""
@@ -115,7 +140,7 @@ class TrafficLights:
 | |¯¯¯¯| |
 | |____| |
 |        |
-{activate_light(ansi_color=GREEN_ANSI)}
+{self.__activate_light__(ansi_color=GREEN_ANSI)}
 |________|
 """
         print(self.green_light)
