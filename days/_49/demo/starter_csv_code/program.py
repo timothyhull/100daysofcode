429f0f4e148d121cde4054f8176e45c1c0584c91
Timothy Hull <timothyhull@gmail.com>
2021-12-04 07:23:13 +0000

    Day 49a

--- days/_49/demo/starter_csv_code/program.py
+++ days/_49/demo/starter_csv_code/program.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python3
 import research
+import cProfile
+
+profiler = cProfile.Profile()
+profiler.disable()
 
 
 def main():
     print("Weather research for Seattle, 2014-2015")
     print()
+
+    # Enable the profiler
+    profiler.enable()
+
     research.init()
 
     print("The hottest 5 days:")
@@ -22,6 +31,13 @@ def main():
     for idx, d in enumerate(days[:5]):
         print("{}. {} inches of rain on {}".format(idx+1, d.actual_precipitation, d.date))
 
+    # Enable the profiler
+    profiler.disable()
+
 
 if __name__ == '__main__':
-    main()
+    for _ in range(1, 25):
+        main()
+
+    # Display profiler stats, sorted by cumulative time (cumtime)
+    profiler.print_stats(sort='cumtime')
